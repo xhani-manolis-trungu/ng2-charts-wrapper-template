04dadb03359adecfff3e94c29e80ea43acd27212
xhani-manolis-trungu <trmanolis@gmail.com>
2021-09-02 18:32:52 +0300

    Create dynamically dataLabels and datasets based on Inputs

--- src/chart-wrapper/chart-wrapper.component.ts
+++ src/chart-wrapper/chart-wrapper.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { ChartDataService } from '../services/chart-data.service';
 
 @Component({
   selector: 'chart-wrapper',
@@ -10,9 +11,21 @@ import { Label } from 'ng2-charts';
 export class ChartWrapperComponent {
   private data: Array<any> = [];
   private labels: Array<string> = [];
-  private type: string = 'bar'
+  private type: string = 'bar';
+  private DSLabels: string | string[] = null;
+  
+  @Input('datasetLabels') set dataSetLabelsInput(DSLabels: string | string[]) {
+    // Way 1 of checking if object is array
+    // if (Object.prototype.toString.call(DSLabels) === '[object Array]') {
+    //   console.log(`array`);
+    // }
+
+    //  Way 2 of checking if object is array
+    this.createBarChartDataLabels(DSLabels);
+  }
   @Input('data') set dataInput(data: Array<any>) {
     this.data = data;
+    this.createBarChartData(data);
   }
   @Input('labels') set labelsInput(labels: Array<string>) {
     this.labels = labels;
@@ -36,7 +49,39 @@ export class ChartWrapperComponent {
 
   ngOnInit() {
     this.barChartType = this.type;
-    this.barChartLabels = [...this.labels]
-    this.barChartData[0]['data'] = [...this.data]
+    this.barChartLabels = [...this.labels];
+    this.barChartData[0]['data'] = [...this.data];
+  }
+
+  createBarChartData(data: Array<any>) {
+    if (data.every(item => Array.isArray(item))) {
+      data.forEach((item, idx) => {
+        if (idx < data.length - 1) {
+          this.barChartData.push({ data: [], label: '' });
+          this.data = item;
+        }
+
+        this.barChartData[idx]['data'] = item;
+      });
+    } else {
+      this.data = data;
+      this.barChartData[0]['data'] = [...this.data];
+    }
+  }
+
+  createBarChartDataLabels(DSLabels: string | string[]) {
+    if (Array.isArray(DSLabels)) {
+      this.DSLabels = [...DSLabels];
+
+      DSLabels.forEach((label, idx) => {
+        if (idx < DSLabels.length - 1) {
+          this.barChartData.push({ data: [], label: '' });
+        }
+        this.barChartData[idx]['label'] = label;
+      });
+    } else {
+      this.DSLabels = DSLabels;
+      this.barChartData[0]['label'] = this.DSLabels;
+    }
   }
 }
